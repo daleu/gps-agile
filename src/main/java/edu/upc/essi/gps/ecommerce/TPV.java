f9416e77da1e7d4a78a6c83f68ce5b237b691ee8
Eduard Maura <eduard.maura.i@est.fib.upc.edu>
2016-04-28 11:19:28 +0200

    Refactor: Canvi de nom de classe, mes entenedor i acceptat pel stakeholders

--- src/main/java/edu/upc/essi/gps/ecommerce/TPV.java
+++ src/main/java/edu/upc/essi/gps/ecommerce/TPV.java
@@ -3,12 +3,12 @@ package edu.upc.essi.gps.ecommerce;
 /**
  * Created by edu on 28/04/16.
  */
-class TerminalVenda {
+class TPV {
     private String nomEmpleat;
     private String nomTerminal;
     private String nomPoblacio;
 
-    public TerminalVenda(String nomEmpleat, String nomTerminal, String nomPoblacio) {
+    public TPV(String nomEmpleat, String nomTerminal, String nomPoblacio) {
         this.nomEmpleat = nomEmpleat;
         this.nomTerminal = nomTerminal;
         this.nomPoblacio = nomPoblacio;
