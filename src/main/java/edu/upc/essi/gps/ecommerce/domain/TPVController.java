6aa40e67443be62af0235a78819c0fcdd23dadb9
Celia Riera Portatil <celia.riera.brutau@est.fib.upc.edu>
2016-05-12 10:40:54 +0200

    Eliminada classe quadrament

--- src/main/java/edu/upc/essi/gps/ecommerce/domain/TPVController.java
+++ src/main/java/edu/upc/essi/gps/ecommerce/domain/TPVController.java
@@ -20,7 +20,6 @@ public class TPVController {
     //Classe singleton
     private Venda vendaActual;
     private Torn tornActual;
-    private Quadrament quadrament;
     private Devolucio devolucioActual;
     private final DevolucionsServei devolucionsServei = new DevolucionsServei();
     private final VendesServei vendesServei = new VendesServei();
